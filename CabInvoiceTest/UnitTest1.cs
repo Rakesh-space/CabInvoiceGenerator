5e1c26154a63203864b6b891ed24976bd83ef67c
Rakesh-space <rakeshsmusale03@gmail.com>
2021-11-21 14:54:06 +0530

    UC1 for single calculate fare

---
+++ CabInvoiceTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CabInvoiceGeneratorProject;
+using CabInvoiceGenerator;
+
+namespace CabInvoiceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            [TestMethod]
+            void GivenDistanceAndTimeShouldReturnTotalFare()
+            {
+                double expected = 25;
+                double distance = 2.0;
+                int time = 5;
+
+                InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL);
+
+                double fare = invoice.calculateFare(distance, time);
+
+                Assert.AreEqual(expected, fare);
+            }
+
+        }
+    }
+}
